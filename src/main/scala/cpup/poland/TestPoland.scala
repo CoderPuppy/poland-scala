c71bd0a4ef6dbf2f4f06efaaa78c8f97de0d89c9
CoderPuppy <cpup@cpup-laptop>
2014-04-16 14:14:23 -0400

    Use PRuntime instead of BaseRuntime

--- src/main/scala/cpup/poland/TestPoland.scala
+++ src/main/scala/cpup/poland/TestPoland.scala
@@ -1,12 +1,12 @@
 package cpup.poland
 
 import cpup.poland.parser.{Parser, Lexer}
-import cpup.poland.runtime.BaseRuntime
+import cpup.poland.runtime.{PObject, PRuntime, BaseRuntime}
 
 object TestPoland {
 	def main(args: Array[String]) {
 		println("Testing Poland (Scala Runtime)")
-		val runtime = BaseRuntime
+		val runtime = new PRuntime(new PObject)
 		println(Parser.parse(runtime, Lexer.lex("println(\"Hello, World!\")", "test.pd")))
 		println("Test Completed")
 	}
