3c6aec427f30c82ecfcd0ddecc7fa3da49a9b485
CoderPuppy <cpup@cpup-laptop>
2014-04-19 23:04:06 -0400

    Test functions

--- src/main/scala/cpup/poland/runtime/RootGround.scala
+++ src/main/scala/cpup/poland/runtime/RootGround.scala
@@ -1,5 +1,9 @@
 package cpup.poland.runtime
 
+import cpup.poland.runtime.userdata._
+import cpup.poland.runtime.userdata.NativeInstruction
+import cpup.poland.runtime.userdata.PSymbol
+
 
 object RootGround {
 	def create = {
@@ -7,6 +11,21 @@ object RootGround {
 		val polandGround = runtime.root
 		val nil = runtime.nil
 		val ground = polandGround.derive
+
+		polandGround(PSymbol(PRuntime.Names.modifySymbol)) = runtime.createRawFunction(
+			polandGround, new PRawFunction(
+				ground,
+				InstructionSeq(
+					new NativeInstruction((runtime: PRuntime, ground: PObject, receiver: PObject) => {
+						println(ground.hints("send").userdata match {
+							case send: Send => send.msg.args(0).eval(runtime, send.context.ground)
+							case _ => null
+						})
+						runtime.nil
+					})
+				)
+			)
+		)
 
 		runtime.initSymbols(polandGround)
 
